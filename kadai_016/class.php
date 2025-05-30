91e924dad49beefb43c580fb6968ca9b94ce959e
YK423a <mwdjg0@gmail.com>
2025-05-30 17:36:24 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -2,7 +2,7 @@
 class Food {
     private $name;
     private $price;
-    function show_price($name,$price){
+    function show_price(){
         $this->name = $name;
         $this->price = $price;
     }
@@ -12,15 +12,15 @@ class Animal {
     private $name;
     private $height;
     private $weight;
-    function show_height($name,$height,$weight){
+    function show_height(){
         $this->name = $name;
         $this->height = $height;
         $this->weight =$weight;
     }
 }
-$food = new Food();
-$Animal = new Animal();
+$food = new Food('pateto',250);
+$Animal = new Animal('dog',60,5000);
 
-print_r($food -> show_price('pateto',250));
-print_r($Animal -> show_height('dog',60,5000));
+print_r($food -> show_price());
+print_r($Animal -> show_height());
 ?>
